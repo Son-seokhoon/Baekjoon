9e9d9a10d0439b9221e636f2b0c6e988e778e3ed
Son-seokhoon <hun8522@naver.com>
2024-02-15 05:59:40 +0900

    윷놀이

---
+++ 2490.py
@@ -0,0 +1,13 @@
+for i in range(3):
+    a=[]
+    a=list(map(int,input().split()))
+    if sum(a)==0:
+        print("D")
+    elif sum(a)==1:
+        print("C")
+    elif sum(a)==2:
+        print("B")
+    elif sum(a)==3:
+        print("A")
+    elif sum(a)==4:
+        print("E")
